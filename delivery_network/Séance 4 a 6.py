5e609b6bb4f4c594c40d92b41d10d818208fd360
Paul Ronsain <paul.ronsain@ensae.fr>
2023-03-30 10:02:22 +0200

    modif

--- delivery_network/Séance 4 a 6.py
+++ delivery_network/Séance 4 a 6.py
@@ -9,12 +9,14 @@ import itertools as it
 
 data_path = "input/"
 
-file_name_roads= "routes.2.in"
-file_name_trucks = "trucks.1.in"
+file_name_roads= "routes.1.in"
+file_name_trucks = "trucks.0.in"
 
-file_name_network= "network.2.in"
+file_name_network= "network.1.in"
 
 g = graph_from_file(data_path + file_name_network)
+roads = roads_from_file(data_path + file_name_roads)
+trucks = trucks_from_file(data_path + file_name_trucks)
 
 def trucks_from_file(filename) :
     """ INPUT  : a text file routes.x.in 
@@ -36,26 +38,28 @@ Cost_models = np.array([truck[1] for truck in trucks_from_file(data_path + file_
 B = 2.5 * 10**9
 
 def f(X) : 
-     C = np.array([x[1] for x in X])
+     C = np.array([x[1] for x in X])  #C est le vecteur avec que les coûts des camions 
      sum(R-C)
 
 def g(X) : #on note la contrainte g
     C = np.array([x[1] for x in X])
-    res = [B-sum(C)]
+    res = [sum(C)-B]
     for i in range(len(X)):
          src = roads[i][0]
          dest = roads[i][1]
-         truck_power = trucks[i][0]
+         truck_power = X[i][0]
          res.append(g.get_path_with_power(src, dest, truck_power)!=None)
     return(res)
 
 
-   
+   # res est un vecteur composé d'une première fonction contrainte budgétaire (qui doit être négative) et d'une suite de booléens qui doivent tous être True 
+
      
-   
-combinations = list(it.product(trucks, repeat = len(R)))
-for X in combinations : 
-     if g(X)[0] >= 0 and all(g(X)[1:])==True and f(x) > fx:
-          fx = f(X)
-          sol = X
-print(X) 
+def force_brute() : 
+    combinations = list(it.product(trucks, repeat = len(R)))
+    fx = 0 
+    for X in combinations : 
+        if g(X)[0] <= 0 and all(g(X)[1:])==True and f(X) > fx:
+            fx = f(X)
+            sol = X
+    return(X)
